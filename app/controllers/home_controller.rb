5362b5a82a4851881dbbcf504af9be13342a3d10
Jonathan Fisher <jonathanzfisher@gmail.com>
2017-06-28 08:53:00 -0500

    add controller actions for home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
-  
+
   def index
+    @products = Product.all
+
+    @categories = Category.all
+  end
+
+  def category
+    category = Category.find(params[:id])
+    @products = category.products
+
+    @categories = Category.all
   end
 end
